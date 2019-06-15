66488d16af7b7d9557747ec4aab95edd2e2f877e
jurahf <jurahf@gmail.com>
2019-06-16 01:18:06 +0500

    Главная страница

--- ImmortalVentureService/ImmortalVentureView/Controllers/HomeController.cs
+++ ImmortalVentureService/ImmortalVentureView/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace ImmortalVentureView.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult MedicalView()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
